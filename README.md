5ed4909d6836f3a7f3ad1c7264cac65238c9c3f4
Yamil Elías <yamileliassoto@gmail.com>
2016-08-05 18:34:41 -0500

    Changes to displaying blocks on home page CSS

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # InmobigammaSite
-Site for client Inmobiliaria GAMMA, developed by SOSTECH
+Site for client Inmobiliaria GAMMA, developed by SOSTECH. The template was taken from realstatepro Theme developed by LivingWandsworth Ltd. THere is no intention on copyright infrigement.
--- page-home.php
+++ page-home.php
@@ -98,10 +98,11 @@
 
 		<div class="col-xs-12 col-sm-12 col-md-12">
 			
-			<h1>Propiedades de búsqueda</h1>
+
 
 				<div class="home-page-search-form">
 
+					<h1>Propiedades de búsqueda</h1>
 					<?php 
 						$formwidth 			= "home-form-width";
 						$inputclass			= "homepage-format";
@@ -117,84 +118,90 @@
 
 		</div>
 
+		<?php if(the_content()) : ?>
 		<div class="col-xs-12 col-sm-12 col-md-12 home-page-content">
-
-			<?php the_content(); ?>
+			
+			<div class="content-block">
+				<?php the_content(); ?>
+			</div>
 
 		</div>
+		<?php endif;?>
 
-		<div class="col-xs-12 col-sm-12 col-md-12">
-			<hr>
-			<h1>Últimas Propiedades a la Venta</h1>
+		<div class="col-xs-12 col-sm-12 col-md-12 latest-property">
+			<!--<hr>-->
+			<div class="content-block">
+				<h1>Últimas Propiedades a la Venta</h1>
 
-			<?php 
+				<?php
 
-			$sale_properties = array (
-				'post_type' 	 => 'properties',
-				'posts_per_page' => 4,
-				'meta_key'		=> 'sale_or_rent',
-				'meta_value'	=> 'sale'
-			);
+				$sale_properties = array (
+					'post_type' 	 => 'properties',
+					'posts_per_page' => 4,
+					'meta_key'		=> 'sale_or_rent',
+					'meta_value'	=> 'sale'
+				);
 
-			$featured_buys = new WP_Query( $sale_properties );
+				$featured_buys = new WP_Query( $sale_properties );
 
-			while ( $featured_buys->have_posts() ) : $featured_buys->the_post(); ?>
-			
-			<div class="col-xs-12 col-sm-6 col-md-3 no-margin-left">
+				while ( $featured_buys->have_posts() ) : $featured_buys->the_post(); ?>
+
+				<div class="col-xs-12 col-sm-6 col-md-3 no-margin-left">
+
+						<div class="featured-properties">
+							<div style="position: relative;">
+								<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><img src="<?php the_field('property_image_1');?>"></a>
+								<span class="featuredlabel"><?php the_title(); ?></span>
+							</div>
+							<h3><?php echo $GLOBALS['theme_settings']['currency'].number_format(get_field('property_price'), 0); ?></h3>
+							<h3><a class="unstyledlink" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_field('sub_title'); ?></a></h3>
+							<p><?php echo substr(get_field('property_details_excerpt'),0,90); ?>...<?php echo sprintf('<a href="%s">Más detalles', esc_url( get_permalink() )),'</a>';?></p>
+						</div>
+				</div>
+
+				<?php endwhile;
+
+				wp_reset_query(); ?>
+			</div>
+		</div>
+		<br/>
+		<div class="col-xs-12 col-sm-12 col-md-12 latest-property">
+
+			<div class="content-block">
+				<h1>Últimas Propiedades en Renta</h1>
+
+				<?php
+
+				$sale_properties = array (
+					'post_type' 	 => 'properties',
+					'posts_per_page' => 4,
+					'meta_key'		=> 'sale_or_rent',
+					'meta_value'	=> 'rent'
+				);
+
+				$featured_buys = new WP_Query( $sale_properties );
+
+				while ( $featured_buys->have_posts() ) : $featured_buys->the_post(); ?>
+
+				<div class="col-xs-12 col-sm-6 col-md-3 no-margin-left">
 
 					<div class="featured-properties">
 						<div style="position: relative;">
 							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><img src="<?php the_field('property_image_1');?>"></a>
 							<span class="featuredlabel"><?php the_title(); ?></span>
-						</div>				
-						<h3><?php echo $GLOBALS['theme_settings']['currency'].number_format(get_field('property_price'), 0); ?></h3>
+						</div>
+						<h3><?php echo $GLOBALS['theme_settings']['currency'].number_format(get_field('property_price'), 0).$GLOBALS['theme_settings']['rental_period']; ?></h3>
 						<h3><a class="unstyledlink" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_field('sub_title'); ?></a></h3>
-						<p><?php echo substr(get_field('property_details_excerpt'),0,90); ?>...<?php echo sprintf('<a href="%s">Más detalles', esc_url( get_permalink() )),'</a>';?></p>
+						<p><?php echo substr(get_field('property_details_excerpt'),0,90); ?>...<?php echo sprintf('<a href="%s">More details', esc_url( get_permalink() )),'</a>';?></p>
 					</div>
-			</div>
 
-			<?php endwhile; 
-
-			wp_reset_query(); ?>
-
-		</div>
-
-		<div class="col-xs-12 col-sm-12 col-md-12">
-			<hr>
-			<h1>Últimas Propiedades en Renta</h1>
-
-			<?php 
-
-			$sale_properties = array (
-				'post_type' 	 => 'properties',
-				'posts_per_page' => 4,
-				'meta_key'		=> 'sale_or_rent',
-				'meta_value'	=> 'rent'
-			);
-
-			$featured_buys = new WP_Query( $sale_properties );
-
-			while ( $featured_buys->have_posts() ) : $featured_buys->the_post(); ?>
-			
-			<div class="col-xs-12 col-sm-6 col-md-3 no-margin-left">
-
-				<div class="featured-properties">
-					<div style="position: relative;">
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><img src="<?php the_field('property_image_1');?>"></a>
-						<span class="featuredlabel"><?php the_title(); ?></span>
-					</div>	
-					<h3><?php echo $GLOBALS['theme_settings']['currency'].number_format(get_field('property_price'), 0).$GLOBALS['theme_settings']['rental_period']; ?></h3>
-					<h3><a class="unstyledlink" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_field('sub_title'); ?></a></h3>
-					<p><?php echo substr(get_field('property_details_excerpt'),0,90); ?>...<?php echo sprintf('<a href="%s">More details', esc_url( get_permalink() )),'</a>';?></p>
 				</div>
 
+
+				<?php endwhile;
+
+				wp_reset_query(); ?>
 			</div>
-
-
-			<?php endwhile; 
-
-			wp_reset_query(); ?>
-
 		</div>
 
 	</div>
